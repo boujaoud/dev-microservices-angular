48ec1ca377f0b2eb4cee9b37f55408d35c489c74
JAMAL_BOUJAOUD <jamal.boujaoud@gmail.com>
2020-01-02 16:03:34 +0100

    [ARCHI-FRONT]:code review

--- jb_front/jbo-angular-vf/e2e/src/app.e2e-spec.ts
+++ jb_front/jbo-angular-vf/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('jbAngular8Front app is running!');
+    expect(page.getTitleText()).toEqual('jbo-angular-vf app is running!');
   });
 
   afterEach(async () => {
