a471a357fbd940890da304b43040df343cd093bd
JAMAL_BOUJAOUD <jamal.boujaoud@gmail.com>
2020-01-05 23:38:13 +0100

    [Spring Cloud]mise en place du micro service customer

---
+++ jb_front/jbo-angular-vf/src/app/jb-app/jb-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jb-app',
+  templateUrl: './jb-app.component.html',
+  styleUrls: ['./jb-app.component.scss']
+})
+export class JbAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
